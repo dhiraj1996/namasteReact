72c63875a31a3b74f8aab53a61d069cc6c8058b9
dhiraj1996 <dhirajkumar1996@gmail.com>
2025-01-04 18:38:05 +0530

    initial code

---
+++ Epsiode 01 - Inception/app.js
@@ -0,0 +1,6 @@
+const heading = document.createElement("h1");
+    heading.innerHTML = "Hello World from Javascript";
+
+    const root = document.getElementById("root")
+
+    root.appendChild(heading);
